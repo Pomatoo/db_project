43a5893f2f1aa37a81aee673b37665c0746bf318
Pomatoo <lky97@outlook.com>
2020-11-18 17:00:08 +0800

    changed config to server path

--- flask-app/book_management_app/config.py
+++ flask-app/book_management_app/config.py
@@ -3,8 +3,8 @@ from configparser import ConfigParser
 
 class Config:
     cf = ConfigParser()
-    cf.read('./web_config.conf')
-    # cf.read('/etc/web_conf.conf')
+    # cf.read('./web_config.conf')
+    cf.read('/etc/web_conf.conf')
     mysql = cf.get('db', 'mysql')
     mongo = cf.get('db', 'mongo')
 
