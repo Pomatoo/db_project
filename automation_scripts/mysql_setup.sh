49422e8903bb3ef3a051cdf16063e8e221203e70
Pomatoo <lky97@outlook.com>
2020-11-18 11:28:33 +0800

    updated mysql,web sh

--- automation_scripts/mysql_setup.sh
+++ automation_scripts/mysql_setup.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-
-sudo apt update && sudo apt upgrade -y
-sudo apt install mysql-server wget unzip -y
-
+#!/usr/bin/env bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password isTdSoo43'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password isTdSoo43'
+sudo apt-get -y install mysql-server
+sudo apt-get -y install wget unzip
 wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/kindle-reviews.zip -O kindle-reviews.zip
 unzip kindle-reviews.zip
 rm -rf kindle_reviews.json kindle-reviews.zip
-
-
-sudo mysql -u root <<MYSQL_SCRIPT
+sudo systemctl start mysql
+sudo mysql -u root -pisTdSoo43<<'EOF'
 CREATE USER 'root'@'%' IDENTIFIED BY 'iStD-So.043-Database';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
@@ -34,6 +34,8 @@ CREATE TABLE testDB.user(
 PRIMARY KEY (`id`),
 INDEX idx_username (username)
 );
-LOAD DATA LOCAL INFILE 'kindle_reviews.csv' INTO TABLE review FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-MYSQL_SCRIPT
-
+LOAD DATA LOCAL INFILE 'kindle_reviews.csv' INTO TABLE testDB.review FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+EOF
+sudo chmod 777 /etc/mysql/my.cnf
+sudo echo "bind-address = 0.0.0.0" >> /etc/alternatives/my.cnf
+sudo systemctl restart mysql
--- automation_scripts/web_setup.sh
+++ automation_scripts/web_setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install python3-pip git python3-venv -y
+sudo apt-get install libmysqlclient-dev python3-pip git python3-venv -y
 git clone "https://github.com/Pomatoo/db_project.git"
-python3.8 -m venv ~/db_project/flask-app/venv
+python3 -m venv ~/db_project/flask-app/venv
 source ~/db_project/flask-app/venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r ~/db_project/flask-app/requirements
-pip3 install PyMySQL pymongo boto3 Flask Flask-WTF Flask-Bcrypt Flask-Login Flask-PyMongo Flask-SQLAlchemy fabric WTForms -y
-python3.8 ~/db_project/flask-app/run.py
+pip3 install PyMySQL pymongo boto3 Flask Flask-WTF Flask-Bcrypt Flask-Login Flask-PyMongo Flask-SQLAlchemy fabric WTForms
+nohup python3 ~/db_project/flask-app/run.py &
