ea72e7c871f3266df35b435eb49ac1be3f00830d
evansusu <57264858+evansusu@users.noreply.github.com>
2020-11-20 12:16:36 +0800

    added pagination for homepage

--- flask-app/book_management_app/__init__.py
+++ flask-app/book_management_app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 from flask_login import LoginManager
 
 # __init__ is where we initialize our application and components
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/testDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:iStD-So.043-Database@34.72.136.99/testDB'
+app.config["MONGO_URI"] = 'mongodb://admin:iStD-So.043-Database@34.72.136.99:27017/test?authSource=admin'
 app.config['SECRET_KEY'] = 'isTdSoo43'
+
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
--- flask-app/book_management_app/routes.py
+++ flask-app/book_management_app/routes.py
@@ -1,4 +1,4 @@
-from book_management_app import app, db, bcrypt, login_manager
+from book_management_app import app, db, bcrypt, login_manager, mongo
 from book_management_app.models import Review, User
 from book_management_app.forms import RegistrationForm, LoginForm, SearchForm
 from flask import request, render_template, redirect, flash, url_for
@@ -12,13 +12,44 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
+@app.route("/", defaults={'page_num': 1, 'page_size': 12})
+@app.route("/<page_num>/<page_size>", methods=['GET', 'POST'])
+def home(page_size, page_num):
     form = SearchForm()
+    page_num = int(page_num)
+    page_size = int(page_size)
+    page_numbers = list(range(1, 4000))
+    skips = page_size * (page_num - 1)
+    book_meta = mongo.db.book_meta.find().skip(skips).limit(page_size)
+    # i = 0
+    book_list = []
+    for book in book_meta:
+        #     i += 1
+        #     if i == 10:
+        #         break
+        book_list.append(book)
+
     if form.validate_on_submit():
         print(form.keyword.data)
         print(form.type.data)
-    return render_template('home.html', form=form)
+
+        if form.type.data.lower() == 'asin':
+
+            book_meta = mongo.db.book_meta.find_one({'asin': form.keyword.data})
+            if book_meta:
+                print(book_meta)
+                reviews = Review.query.filter_by(asin=form.keyword.data).all()
+                print(type(reviews))
+                print('reviews size : %s ' % len(reviews))
+                for i in reviews:
+                    print(reviews)
+            else:
+                flash('Book %s not found' % form.keyword.data, 'danger')
+        elif form.type.data.lower() == 'book':
+            pass
+
+    return render_template('home.html', form=form, books=book_list, page_numbers=page_numbers, page_size=page_size,
+                           page_num=page_num)
 
 
 @app.route("/about", methods=['GET'])
@@ -30,6 +61,10 @@ def about():
 @login_required
 def management():
     if current_user.username == 'admin':
+        book_meta = mongo.db.book_meta.find()
+        book_list = []
+        for book in book_meta:
+            book_list.append(book)
         return render_template('management.html')
     else:
         flash('Please login as admin and try again', 'danger')
@@ -71,3 +106,12 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/book/<asin>")
+def book(asin):
+    book_meta = mongo.db.book_meta.find()
+    book_list = []
+    for book in book_meta:
+        book_list.append(book)
+        return render_template('book.html', title='book', book_list=book_list)
